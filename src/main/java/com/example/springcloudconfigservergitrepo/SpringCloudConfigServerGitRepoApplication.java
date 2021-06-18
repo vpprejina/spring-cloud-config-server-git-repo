904bceea6950365365ddae6933a7a9542edad3d6
vpprejina <vpprejina@gmail.com>
2021-06-18 17:19:52 -0400

    for deploy

--- src/main/java/com/example/springcloudconfigservergitrepo/SpringCloudConfigServerGitRepoApplication.java
+++ src/main/java/com/example/springcloudconfigservergitrepo/SpringCloudConfigServerGitRepoApplication.java
@@ -2,14 +2,21 @@ package com.example.springcloudconfigservergitrepo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class SpringCloudConfigServerGitRepoApplication {
+public class SpringCloudConfigServerGitRepoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudConfigServerGitRepoApplication.class, args);
 	}
-
+@Override
+   protected SpringApplicationBuilder configure (SpringApplicationBuilder builder) {
+	return builder.sources(SpringCloudConfigServerGitRepoApplication.class);
+	
+	
+}
 }
