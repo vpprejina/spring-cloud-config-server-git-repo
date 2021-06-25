972a9f1c4cfe35d01a0b149fdae4e7fd515d9d8e
vpprejina <vpprejina@gmail.com>
2021-06-25 14:56:29 -0400

    updated

--- src/main/java/com/example/springcloudconfigservergitrepo/SpringCloudConfigServerGitRepoApplication.java
+++ src/main/java/com/example/springcloudconfigservergitrepo/SpringCloudConfigServerGitRepoApplication.java
@@ -5,17 +5,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 @EnableConfigServer
-public class SpringCloudConfigServerGitRepoApplication extends SpringBootServletInitializer  {
+public class SpringCloudConfigServerGitRepoApplication {
 
-	@Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(SpringCloudConfigServerGitRepoApplication.class);
-    }
+
+	
+	@GetMapping("/test")
+	public String test() {
+		return "Application Deployed by rejina";
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudConfigServerGitRepoApplication.class, args);
 	}
-
 }
--- src/test/java/com/example/springcloudconfigservergitrepo/SpringCloudConfigServerGitRepoApplicationTests.java
+++ src/test/java/com/example/springcloudconfigservergitrepo/SpringCloudConfigServerGitRepoApplicationTests.java
@@ -3,6 +3,7 @@ package com.example.springcloudconfigservergitrepo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class SpringCloudConfigServerGitRepoApplicationTests {
 
